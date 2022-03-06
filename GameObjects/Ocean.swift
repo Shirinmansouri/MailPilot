ec40b5248aa7a4770b2c98334fc3369cc7195385
Shirin <shirin.mansori@gmail.com>
2022-03-06 12:28:44 -0500

    First Commit

---
+++ GameObjects/Ocean.swift
@@ -0,0 +1,49 @@
+import GameplayKit
+import SpriteKit
+
+class Ocean : GameObject
+{
+    
+    // constructor / initializer
+    init()
+    {
+        super.init(imageString: "ocean-1", initialScale: 2.0)
+        Start()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // LifeCycle Functions
+    override func CheckBounds()
+    {
+        if(position.x <= -773)
+        {
+            Reset()
+        }
+    }
+    
+    override func Reset()
+    {
+        position.x = 773
+    }
+    
+    // initialization
+    override func Start()
+    {
+        zPosition = 0
+        verticalSpeed = 5.0
+    }
+    
+    override func Update()
+    {
+        Move()
+        CheckBounds()
+    }
+    
+    func Move()
+    {
+        position.x -= verticalSpeed!
+    }
+}
